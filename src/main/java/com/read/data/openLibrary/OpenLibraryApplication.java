7a6ee68fcf36b75c0e9a1a1785e5aaef00444277
Ravindra Reddy Ambati <ravindra.reddy.ambati@outlook.in>
2022-09-13 22:52:41 +0530

    spring init

---
+++ src/main/java/com/read/data/openLibrary/OpenLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.read.data.openLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/read/data/openLibrary/OpenLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.read.data.openLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
